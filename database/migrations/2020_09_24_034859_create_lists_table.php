2d6e14c29bbd27561766215c2d9390215f23f460
Haresh <haresh.v521@gmail.com>
2020-09-25 11:26:40 +0530

    Laravel React demo code

---
+++ database/migrations/2020_09_24_034859_create_lists_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('position', ['left', 'right']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lists');
+    }
+}
